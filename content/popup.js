f2f8f6580cfaaac760af8fd223ffa3290c964b69
Chewsterchew <chew@chew.pw>
2017-09-07 15:46:29 -0500

    upload what exists

---
+++ content/popup.js
@@ -0,0 +1,10 @@
+var i = 1;
+if(localStorage['currentIndex'])
+	i = localStorage['currentIndex'];
+var cat = "http://www.iscalio.com/cats/"+i+".jpg";
+if(!localStorage['preloadedCat'] || localStorage['preloadedCat'] == "")
+	document.getElementById("cat").src = cat
+else
+	document.getElementById("cat").src = localStorage['preloadedCat'];
+
+localStorage['currentEntryAccessed'] = 1;
